415faecafe44f9fd22ff6f51c11fbc0174bc2362
mahadi609im <maha609im@gmail.com>
2025-11-08 21:12:11 -0600

    pinged to connect mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +8,36 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// pawMartUser;
+// aXGxzkOuwxetTAZL;
+const uri =
+  'mongodb+srv://pawMartUser:aXGxzkOuwxetTAZL@cluster0.4k43auc.mongodb.net/?appName=Cluster0';
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get('/', (req, res) => {
   res.send('Bismillah');
 });
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //  await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
