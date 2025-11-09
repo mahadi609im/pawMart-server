48e65464c4bb162baa2f58835b220beb0b864017
mahadi609im <maha609im@gmail.com>
2025-11-08 22:09:11 -0600

    delete listings data

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/listings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await listingCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
