ea43d1a50cceee311e5f2b8a7c213d6e4aa7cd4a
mahadi609im <maha609im@gmail.com>
2025-11-13 00:45:27 -0600

    problem fix

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const pawMartDB = client.db('pawMartDB');
 
     //   ---Listings---
